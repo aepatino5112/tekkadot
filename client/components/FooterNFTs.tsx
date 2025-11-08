bc70286f7f31b83ee0c26329c78a2c2d02c84cf1
aepatino5112 <aepatino5112@gmail.com>
2025-11-08 19:07:41 -0400

    feat: add nfts footer component

---
+++ client/components/FooterNFTs.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const FooterNFTs = () => {
+
+    return (
+        <footer className="footer">
+            <div className="relative flex flex-col items-start">
+                <Image src="/logos/tekka-green.svg" alt="TekkaDot nfts light logo" width={278} height={75.14} className="block pb-9 dark:hidden" />
+                <Image src="/logos/tekka-green-dark.svg" alt="TekkaDot nfts dark logo" width={278} height={75.14} className="hidden pb-9 dark:block" />
+                <p className="text-300 font-normal leading-7 text-black-500 dark:text-white-500">We are a tech shop dedicated to offer you the</p>
+                <p className="text-300 font-normal leading-7 pb-6 text-black-500 dark:text-white-500">best tech for the best prices in the market.</p>
+                <p className="paragraph-lg font-light text-black-500 dark:text-white-500">© 2025 TekkaDot. All rights reserved.</p>
+            </div>
+            <div className="flex justify-between items-start gap-30">
+                <div className="footer-item mr-30">
+                    <h5 className="text-lime-green-500 font-bold">MARKETPLACE</h5>
+                    <Link href="products"><p className="paragraph-lg font-light text-black-500 dark:text-white-500">Products</p></Link>
+                    <Link href="nfts"><p className="paragraph-lg font-light text-black-500 dark:text-white-500">NFTs</p></Link>
+                    <Link href="/tutorial"><p className="paragraph-lg font-light text-black-500 dark:text-white-500">How It Works</p></Link>
+                </div>
+                <div className="footer-item mr-30">
+                    <h5 className="text-lime-green-500 font-bold">ABOUT US</h5>
+                    <Link href="about-us"><p className="paragraph-lg font-light text-black-500 dark:text-white-500">Who are we</p></Link>
+                    <Link href="/mission"><p className="paragraph-lg font-light text-black-500 dark:text-white-500">Our mission</p></Link>
+                    <Link href="/vission"><p className="paragraph-lg font-light text-black-500 dark:text-white-500">Our vission</p></Link>
+                    <Link href="/allies"><p className="paragraph-lg font-light text-black-500 dark:text-white-500">Our allies</p></Link>
+                </div>
+                <div className="footer-item mr-30">
+                    <h5 className="text-lime-green-500 font-bold">LEGAL</h5>
+                    <Link href="/service-terms"><p className="paragraph-lg font-light text-black-500 dark:text-white-500">Terms of Service</p></Link>
+                    <Link href="/privacy-policy"><p className="paragraph-lg font-light text-black-500 dark:text-white-500">Privacy Policy</p></Link>
+                    <Link href="/disclaimer"><p className="paragraph-lg font-light text-black-500 dark:text-white-500">Disclaimer</p></Link>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default FooterNFTs;
