1d1ba4ade67c8ee70e5f0437c62dfbd465c57fcf
aepatino5112 <aepatino5112@gmail.com>
2025-11-10 12:42:03 -0400

    feat: add All Products tag in Featured Products component

--- client/components/FeaturedProducts.tsx
+++ client/components/FeaturedProducts.tsx
@@ -1,11 +1,19 @@
+import Link from "next/link";
 import ProductCard from "./ProductCard";
 import { type FeaturedProductsProps } from "@/types/cards";
+import { MoveUpRight } from 'lucide-react';
 
 const FeaturedProducts = ({ products  }: FeaturedProductsProps ) => {
 
     return (
         <section className="flex flex-col justify-start mt-73 mb-[4rem] gap-[3.5rem]">
-            <h2 className="text-black-500 dark:text-white-500 font-semibold">Featured Products</h2>
+            <div className="flex justify-between">
+                <h2 className="text-black-500 dark:text-white-500 font-semibold">Featured Products</h2>
+                <Link href="/products" className="flex items-center gap-1.5">
+                    <p className="text-[2rem] font-medium text-vivid-pink-600 dark:text-vivid-pink-400 underline-hover">All Products</p>
+                    <MoveUpRight className="w-[1.896rem] h-[1.799rem] text-vivid-pink-600 dark:text-vivid-pink-400" />
+                </Link>
+            </div>
             <div className="flex justify-between items-center gap-[1.5rem]">
                 {products.map((product) => (
                     <ProductCard 
