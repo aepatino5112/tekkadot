52327b7eabd82857d4e6bad7fe58cd81ed4fb337
aepatino5112 <aepatino5112@gmail.com>
2025-11-10 23:20:45 -0400

    feat: add initial background for the newsletter

---
+++ client/components/Newsletter.tsx
@@ -0,0 +1,11 @@
+
+const Newsletter = () => {
+
+    return (
+        <div className="w-412 h-140.5 rounded-2xl bg-[url('/images/newsletter.jpg')] bg-cover bg-center">
+
+        </div>
+    );
+};
+
+export default Newsletter;
