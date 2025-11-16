a9f94d81e145a492500b928291e7dc7019a387f8
SebastianGomes03 <sagomes.22@est.ucab.edu.ve>
2025-11-15 20:25:52 -0400

    feat: fixes

--- client/components/Collections.tsx
+++ client/components/Collections.tsx
@@ -7,7 +7,7 @@ const Collections = () => {
         <h2 className="text-xl md:text-2xl lg:text-3xl font-medium text-black-500 dark:text-white-500">
           Collections
         </h2>
-        <div className="flex flex-wrap justify-between gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-4.5">
           <CollectionCard name="gaming" />
           <CollectionCard name="laptop" />
           <CollectionCard name="mobile" />
