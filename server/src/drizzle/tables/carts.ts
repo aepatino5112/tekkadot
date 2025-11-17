f7fea7ced354602a4ae17ef89396c3e2b093391e
aepatino5112 <aepatino5112@gmail.com>
2025-11-17 00:45:48 -0400

    feat: add supabase support

--- server/src/drizzle/tables/carts.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
-import { UsersTable } from './users.js';
-
-export const CartsTable = pgTable('carts', {
-    cart_id: uuid('cart_id').defaultRandom().primaryKey(),
-    created_at: timestamp('created_at').defaultNow().notNull(),
-    updated_at: timestamp('updated_at').defaultNow().notNull(),
-    user_id: uuid('user_id').references(() => UsersTable.user_id).notNull()
-});
--- server/src/drizzle/tables/users.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
-
-export const UsersTable = pgTable('users', {
-    user_id: uuid('user_id').primaryKey().defaultRandom(),
-    created_at: timestamp('created_at').defaultNow().notNull()
-});
